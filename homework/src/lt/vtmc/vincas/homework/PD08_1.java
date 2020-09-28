893555e9f59ed614207dc67e0896d98c8e761132
Aloyzas <Aloyzas@DESKTOP-NN02FF1>
2020-09-28 12:30:11 +0300

    Namu darbas 8, uzduotis 1

---
+++ homework/src/lt/vtmc/vincas/homework/PD08_1.java
@@ -0,0 +1,37 @@
+package lt.vtmc.vincas.homework;
+
+
+import java.util.Scanner;
+
+public class PD08_1 {
+
+	public static void main(String[] args) {
+		
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Iveskite parduotu prekiu kieki: ");
+		int a = input.nextInt();			// a - prekiu kiekis
+		
+		double prices[] = new double[a];
+		
+		
+		System.out.println("Iveskite prekiu kainas: ");
+		for (int i = 0; i < a; i++) {
+			prices[i] = input.nextDouble();
+		}
+		
+		double sum = 0;
+		for (int i = 0; i < a ; i++){
+			sum += prices[i];
+							}
+		double avgPrice = sum / a;
+		System.out.println("Vidutiniskai viena preke kainavo " + avgPrice + " Eur.");
+		System.out.println("Per diena prekiu parduota uz " + sum + " Eur"   );
+		  
+			 
+
+	}
+}
+
+		
